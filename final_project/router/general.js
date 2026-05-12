4433cb82a476c80e8786785157c9cda6d31671ac
Tosinuel <expressionandpuresound@gmail.com>
2026-05-12 13:52:36 +0100

    Minor addition

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,7 +23,7 @@ const getBooksByAuthorPromise = (author) => {
   });
 };
 
-// Helper function to get book by title using Axios with Promise
+// Helper function to get book by title using Axios with Promise.
 const getBooksByTitlePromise = (title) => {
   return new Promise((resolve, reject) => {
     axios.get('http://localhost:5000/title/' + title)
